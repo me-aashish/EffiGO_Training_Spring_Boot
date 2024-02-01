2f3bfbb3da070c0dcc4a3b18ad9ff25851a90cdf
Aashish Kumar Jha <20bcs3872@cuchd.in>
2024-02-01 10:58:50 +0530

    created a basic GET rest api - /courses

---
+++ src/main/java/com/aashish/springboot/springbootgettingstarted/SpringBootGettingStartedApplication.java
@@ -0,0 +1,14 @@
+package com.aashish.springboot.springbootgettingstarted;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGettingStartedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGettingStartedApplication.class, args);
+
+	}
+
+}
